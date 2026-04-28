39e5a2bdbe7ce5f2f2caa4f54de589135b35740b
Maximo Astudillo <maximastudillo9@gmail.com>
2026-04-28 01:18:41 -0500

    creacion de componentes Login

--- scaf-client/src/app/app.ts
+++ scaf-client/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './security/component/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

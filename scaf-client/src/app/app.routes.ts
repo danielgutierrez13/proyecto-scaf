352277ab2e74c932f5c663060d89ec4d1d2327c9
Benjamin-gutierrez <benja.gutierrezvillegas@gmail.com>
2026-04-28 03:17:58 -0500

    Agregar diseño y estructura de la página de layout.

--- scaf-client/src/app/app.routes.ts
+++ scaf-client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './security/component/login/login';
+import { Layout } from './layout/component/layout/layout';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: Login },
+  { path: 'layout', component: Layout },
   { path: '**', redirectTo: '/login' }
 ];

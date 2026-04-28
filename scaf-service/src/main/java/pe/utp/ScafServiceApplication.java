77d98bc198a6f6c29c7cf9b2dab2fe85c7e9db9b
Benjamin-gutierrez <benja.gutierrezvillegas@gmail.com>
2026-04-27 23:31:32 -0500

    inicialización proyecto backend

---
+++ scaf-service/src/main/java/pe/utp/ScafServiceApplication.java
@@ -0,0 +1,13 @@
+package pe.utp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScafServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScafServiceApplication.class, args);
+	}
+
+}

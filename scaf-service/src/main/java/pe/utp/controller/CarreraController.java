c0e077a46d517de960374bfbd654f799eac35299
Benjamin Alexander Gutierrez Villegas <benja.gutierrezvillegas@gmail.com>
2026-05-22 01:31:09 -0500

    ajustes a los controladores

--- scaf-service/src/main/java/pe/utp/controller/CarreraController.java
+++ scaf-service/src/main/java/pe/utp/controller/CarreraController.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +14,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import pe.utp.repository.model.Carrera;
 import pe.utp.service.CarreraService;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/carreras")
 @CrossOrigin(origins = "http://localhost:4200")
--- scaf-service/src/main/java/pe/utp/controller/UsuarioController.java
+++ scaf-service/src/main/java/pe/utp/controller/UsuarioController.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +14,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import pe.utp.dto.UsuarioRequestDto;
 import pe.utp.dto.UsuarioResponseDto;
 import pe.utp.service.UsuarioService;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/usuarios")
 @CrossOrigin(origins = "http://localhost:4200")
